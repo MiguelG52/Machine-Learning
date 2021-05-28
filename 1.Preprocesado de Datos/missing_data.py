db473b658e393e248343d8cbdb668bc1173aa4f8
Miguel González <53835554+MiguelG52@users.noreply.github.com>
2021-05-27 20:17:31 -0500

    Templates
    Se crean templates de python, para 3 diferentes tipos de muestras de datos

--- 1.Preprocesado de Datos/missing_data.py
+++ 1.Preprocesado de Datos/missing_data.py
@@ -1,7 +1,3 @@
-#plantilla de procesado
-#@author: MiguelG52
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -12,8 +8,13 @@ dataset = pd.read_csv('Data.csv')
 x = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,3].values
 
+#tratamiento de NAS
+from sklearn.impute import SimpleImputer
+#axis = o pide datos de columna, axis = 1 de fila
+imp = SimpleImputer(missing_values = 'NaN', strategy = 'mean')
+imp = SimpleImputer().fit(x[:, 1:3])
+x[:, 1:3] = imp.transform(x[:, 1:3])
 
-#dividir dataset  en conjunto de entrenamiento y testing
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.2, random_state = 0)
 
@@ -22,3 +23,5 @@ from sklearn.preprocessing import StandardScaler
 sc_x = StandardScaler()
 x_train =  sc_x.fit_transform(x_train)
 x_test = sc_x.transform(x_test)
+
+
